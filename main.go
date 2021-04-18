b3c4c232bb92a6086fa13be553e62989d0e72870
petrostrak <pit.trak@gmail.com>
2021-04-18 21:07:46 +0300

    impl BroadcastMessage

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"os"
+	"sync"
 
 	"github.com/petrostrak/Chat-application-gRPC-docker/proto"
 	glog "google.golang.org/grpc/grpclog"
@@ -36,6 +38,37 @@ func (s *Server) CreateStream(pconn *proto.Connect, stream proto.Broadcast_Creat
 
 	s.Connection = append(s.Connection, conn)
 	return <-conn.error
+}
+
+func (s *Server) BroadcastMessage(ctx context.Context, msg *proto.Message) (*proto.Close, error) {
+	wait := sync.WaitGroup{}
+	done := make(chan int)
+
+	for _, conn := range s.Connection {
+		wait.Add(1)
+		go func(msg *proto.Message, conn *Connection) {
+			defer wait.Done()
+
+			if conn.active {
+				err := conn.stream.Send(msg)
+				grpcLog.Info("Sending message to:", conn.stream)
+
+				if err != nil {
+					grpcLog.Errorf("Error with Stream: %s - Error: %v", conn.stream, err)
+					conn.active = false
+					conn.error <- err
+				}
+			}
+		}(msg, conn)
+	}
+
+	go func() {
+		wait.Wait()
+		close(done)
+	}()
+
+	<-done
+	return &proto.Close{}, nil
 }
 
 func main() {
